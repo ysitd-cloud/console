95a52c7b9d8c854ff9383d56fb1fd085f60fd99e
Tony Yip <tony@opensource.hk>
2017-10-16 01:24:46 +0800

    Update for security

--- src/backend/security.js
+++ src/backend/security.js
@@ -1,11 +1,5 @@
 const helmet = require('helmet');
-const { IS_PRODUCTION } = require('./config');
 
 module.exports = (app) => {
-  if (IS_PRODUCTION) {
-    app.disable('x-powered-by');
-  }
-
-  app.use(helmet.noSniff());
-  app.use(helmet.frameguard({ action: 'deny' }));
+  app.use(helmet());
 };
