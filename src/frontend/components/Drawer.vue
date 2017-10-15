a3b5e77402701fd948989d5057c52b9eb5fb5924
Tony Yip <tony@opensource.hk>
2017-10-16 01:24:59 +0800

    Fix rendering

--- src/frontend/components/Drawer.vue
+++ src/frontend/components/Drawer.vue
@@ -7,24 +7,29 @@
                         <img :src="photo" />
                     </v-list-tile-avatar>
                     <v-list-tile-content>
-                        <v-list-tile-title>
-                            {{ displayName }}
-                        </v-list-tile-title>
+                        <v-list-tile-title>{{ displayName }}</v-list-tile-title>
                     </v-list-tile-content>
                 </v-list-tile>
             </v-list>
         </v-toolbar>
         <v-list>
-            <v-list-tile href="https://account.ysitd.cloud/" target="_blank">
-                Account <v-icon>launch</v-icon>
-            </v-list-tile>
-            <v-list-tile href="/auth/logout">
-                Logout
-            </v-list-tile>
+            <template v-for="link in links">
+                <v-list-tile v-if="link.external" :key="link.href" :href="link.href" target="_blank">
+                    <span>{{ link.display }}</span>
+                    <v-spacer />
+                    <v-icon>launch</v-icon>
+                </v-list-tile>
+            </template>
+            <v-list-tile href="/auth/logout">Logout</v-list-tile>
             <v-divider></v-divider>
         </v-list>
     </v-navigation-drawer>
 </template>
+
+<style scoped lang="stylus">
+    .list__tile--link .icon
+        font-size: 1rem
+</style>
 
 <script>
     import { mapState, mapGetters } from 'vuex';
@@ -51,6 +56,14 @@
         ...mapGetters({
           photo: 'user/photo',
         }),
+      },
+      data() {
+        return {
+          links: [
+            { href: 'https://account.ysitd.cloud/', external: true, display: 'Account' },
+            { href: 'https://health.ysitd.cloud/', external: true, display: 'Service Health' },
+          ],
+        };
       },
     };
 </script>
