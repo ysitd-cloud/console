ae75aca8b4872c4abdde23f549d7077ccb906dc1
Tony Yip <tony@opensource.hk>
2017-12-30 02:01:32 +0800

    Add apollo graphql packages

--- build/webpack.client.conf.js
+++ build/webpack.client.conf.js
@@ -11,6 +11,11 @@ module.exports = merge.smart(base, {
   module: {
     rules: [
       {
+        test: /\.graphql$/,
+        exclude: /node_modules/,
+        loader: 'graphql-tag/loader',
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: [
