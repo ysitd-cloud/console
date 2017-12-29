5681d35c7a61b8b0f5cd73a029b52188c4575bb5
Tony Yip <tony@opensource.hk>
2017-12-30 04:01:43 +0800

    Add error card

---
+++ src/frontend/components/ErrorCard.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-card>
+        <v-card-title primary-title>
+            <div>
+                <h3 class="headline mb-0">Error Occur</h3>
+                <div class="red--text">{{ error }}</div>
+            </div>
+        </v-card-title>
+    </v-card>
+</template>
+
+<script>
+  export default {
+    name: 'error-card',
+    props: {
+      error: {
+        type: String,
+        required: true,
+      },
+    },
+  };
+</script>
