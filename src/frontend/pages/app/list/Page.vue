27af9ea8abcae58fd980feb209cbaec9218dbd8a
Tony Yip <tony@opensource.hk>
2018-01-17 04:26:57 +0800

    Fix list app problem when return null problem

--- src/frontend/pages/app/list/Page.vue
+++ src/frontend/pages/app/list/Page.vue
@@ -4,7 +4,7 @@
             <v-card-title>
                 <h3 class="mb-0">App</h3>
             </v-card-title>
-            <v-list v-if="apps.length > 0">
+            <v-list v-if="apps && apps.length > 0">
                 <v-list-tile v-for="app in apps" :key="app.id">
                     <v-list-tile-content>
                         <v-list-tile-title>{{ app.name }}</v-list-tile-title>
