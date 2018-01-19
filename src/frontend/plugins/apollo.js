78d18ab3b2951a7fd4cc881554fe16400c8a9907
Tony Yip <tony@opensource.hk>
2018-01-19 14:26:49 +0800

    Solve problem in token expire

--- src/frontend/plugins/apollo.js
+++ src/frontend/plugins/apollo.js
@@ -2,21 +2,28 @@ import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 import axios from 'axios';
+import * as moment from 'moment';
+
+function tokenExpire(time) {
+  return moment().diff(moment(time), 'hours') > 1;
+}
 
 /* global window */
 class Apollo {
   constructor() {
     this.cache = new InMemoryCache();
     const token = window.sessionStorage.getItem('accessToken');
-    if (!token) {
+    const issue = window.sessionStorage.getItem('tokenIssue');
+    if (!token || !issue || tokenExpire(issue)) {
       this.loadToken();
     } else {
+      this.issue = moment(issue);
       this.token = token;
     }
   }
 
   async getClient() {
-    if (!this.client) {
+    if (!this.client || tokenExpire(this.issue)) {
       await this.createClient();
     }
 
@@ -59,7 +66,9 @@ class Apollo {
     return axios.post('/auth/api')
       .then(({ data }) => {
         this.token = data.token;
+        this.issue = moment();
         window.sessionStorage.setItem('accessToken', data.token);
+        window.sessionStorage.setItem('tokenIssue', this.issue.format());
       });
   }
 }
