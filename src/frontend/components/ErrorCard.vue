6368222280703c40d6621fc0b009f1a4a5e8563f
Tony Yip <tony@opensource.hk>
2018-01-23 16:28:21 +0800

    Change Error Card to functional

--- src/frontend/components/ErrorCard.vue
+++ src/frontend/components/ErrorCard.vue
@@ -1,22 +1,10 @@
-<template>
+<template functional>
     <v-card>
         <v-card-title primary-title>
             <div>
                 <h3 class="headline mb-0">Error Occur</h3>
-                <div class="red--text"><pre><code>{{ error }}</code></pre></div>
+                <div class="red--text"><pre><code>{{ props.error }}</code></pre></div>
             </div>
         </v-card-title>
     </v-card>
 </template>
-
-<script>
-  export default {
-    name: 'error-card',
-    props: {
-      error: {
-        type: String,
-        required: true,
-      },
-    },
-  };
-</script>
