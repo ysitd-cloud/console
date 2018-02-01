6ce2b1e3567fc89f1e5cd0915c7fdb5bf9c46272
Tony Yip <tony@opensource.hk>
2018-02-01 12:35:14 +0800

    Please eslint

--- src/frontend/components/react/CardTitle.jsx
+++ src/frontend/components/react/CardTitle.jsx
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
 export default function CardTitle(props) {
-  const { className, children, ...other } = props
+  const { className, children, ...other } = props;
   const classes = classNames(className, 'card__title');
   return <div className={classes} {...other}>{children}</div>;
 }
